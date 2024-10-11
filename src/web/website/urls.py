71b743768d2fb418c0b7a5fc1846d067b33ab6db
Saqib-DevSecOp <saqibahmad778866@gmail.com>
2024-10-11 17:38:22 +0500

    Website: Pages Design >> home , About , Pricing , Contact Us

--- src/web/website/urls.py
+++ src/web/website/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
 from .views import (
-    HomeView
+    HomeView, AboutView, PricingView, ContactUs
 )
 
 app_name = "website"
 urlpatterns = [
-    path('', HomeView.as_view(), name='home')
+    path('', HomeView.as_view(), name='home'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('pricing/', PricingView.as_view(), name='pricing'),
+    path('contact/', ContactUs.as_view(), name='contact'),
+
 ]
--- src/web/website/views.py
+++ src/web/website/views.py
@@ -7,3 +7,12 @@ from src.apps.whisper.main import NotificationService
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'website/index.html'
+
+class AboutView(TemplateView):
+    template_name = 'website/about.html'
+
+class PricingView(TemplateView):
+    template_name = 'website/pricing.html'
+
+class ContactUs(TemplateView):
+    template_name = 'website/contact.html'
